3f227d44965a5c664c95792f0530d84a6aded4cc
Nico <npintos@gmail.com>
2017-06-24 13:28:44 -0400

    Initial commit

---
+++ demo/src/main/java/net/cachapa/expandablelayoutdemo/HorizontalFragment.java
@@ -0,0 +1,42 @@
+package net.cachapa.expandablelayoutdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import net.cachapa.expandablelayout.ExpandableLayout;
+
+public class HorizontalFragment extends Fragment implements View.OnClickListener, ExpandableLayout.OnExpansionUpdateListener {
+    private ExpandableLayout expandableLayout;
+    private ImageView expandButton;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.horizontal_fragment, container, false);
+
+        expandableLayout = (ExpandableLayout) rootView.findViewById(R.id.expandable_layout);
+        expandButton = (ImageView) rootView.findViewById(R.id.expand_button);
+
+        expandableLayout.setOnExpansionUpdateListener(this);
+        expandButton.setOnClickListener(this);
+
+        return rootView;
+    }
+
+    @Override
+    public void onExpansionUpdate(float expansionFraction, int state) {
+        Log.d("ExpandableLayout", "State: " + state);
+        expandButton.setRotation(expansionFraction * 180);
+    }
+
+    @Override
+    public void onClick(View view) {
+        expandableLayout.toggle();
+    }
+}
---
+++ demo/src/main/java/net/cachapa/expandablelayoutdemo/RecyclerViewFragment.java
@@ -0,0 +1,116 @@
+package net.cachapa.expandablelayoutdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.OvershootInterpolator;
+import android.widget.TextView;
+
+import net.cachapa.expandablelayout.ExpandableLayout;
+
+public class RecyclerViewFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.recycler_view_fragment, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(new SimpleAdapter(recyclerView));
+
+        return rootView;
+    }
+
+    private static class SimpleAdapter extends RecyclerView.Adapter<SimpleAdapter.ViewHolder> {
+        private static final int UNSELECTED = -1;
+
+        private RecyclerView recyclerView;
+        private int selectedItem = UNSELECTED;
+
+        public SimpleAdapter(RecyclerView recyclerView) {
+            this.recyclerView = recyclerView;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.recycler_item, parent, false);
+            return new ViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.bind(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return 100;
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, ExpandableLayout.OnExpansionUpdateListener {
+            private ExpandableLayout expandableLayout;
+            private TextView expandButton;
+            private int position;
+            private TextView t;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+
+                expandableLayout = (ExpandableLayout) itemView.findViewById(R.id.expandable_layout);
+                expandableLayout.setInterpolator(new OvershootInterpolator());
+                expandableLayout.setOnExpansionUpdateListener(this);
+                expandButton = (TextView) itemView.findViewById(R.id.expand_button);
+                t = (TextView) itemView.findViewById(R.id.t);
+
+
+                expandButton.setOnClickListener(this);
+
+                t.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Log.i("click","true");
+                    }
+                });
+            }
+
+            public void bind(int position) {
+                this.position = position;
+
+                expandButton.setText(position + ". Tap to expand");
+
+                expandButton.setSelected(false);
+                expandableLayout.collapse(false);
+            }
+
+            @Override
+            public void onClick(View view) {
+                ViewHolder holder = (ViewHolder) recyclerView.findViewHolderForAdapterPosition(selectedItem);
+                if (holder != null) {
+                    holder.expandButton.setSelected(false);
+                    holder.expandableLayout.collapse();
+                }
+
+                if (position == selectedItem) {
+                    selectedItem = UNSELECTED;
+                } else {
+                    expandButton.setSelected(true);
+                    expandableLayout.expand();
+                    selectedItem = position;
+                }
+            }
+
+            @Override
+            public void onExpansionUpdate(float expansionFraction, int state) {
+                Log.d("ExpandableLayout", "State: " + state);
+                recyclerView.smoothScrollToPosition(getAdapterPosition());
+            }
+        }
+    }
+}
